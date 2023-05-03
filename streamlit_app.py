384f1d75f5db523b76a71efdd21c360b03d8f12b
Darryl070 <103985023+Darryl070@users.noreply.github.com>
2023-05-03 16:22:34 +0800

    Update streamlit_app.py
    troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -43,6 +42,7 @@ try:
 except URLError as e:
     streamlit.error()
     
+streamlit.stop() 
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
